45d1656c3c73cd151df2ae96c14d28ef1b0cca07
rajsimmanravi <rravichandiran@venacorp.com>
2025-01-28 09:58:11 -0500

    Remove azurerm in lb_rule_idle_timeout_in_minutes

--- variables.tf
+++ variables.tf
@@ -153,7 +153,7 @@ variable "pip_idle_timeout_in_minutes" {
   description = "(Optional) Specifies the timeout for the TCP idle connection. The value can be set between 4 and 30 minutes. Defaults to `4`."
 }
 
-variable "azurerm_lb_rule_idle_timeout_in_minutes" {
+variable "lb_rule_idle_timeout_in_minutes" {
   type        = number
   default     = 10
   description = "(Optional) Specifies the timeout for the TCP idle connection. The value can be set between 4 and 30 minutes. Defaults to `4`."
